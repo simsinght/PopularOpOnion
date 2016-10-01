48d95487ebcf6f52615db913d59a463a28cc65eb
Megan <Megan@MacBook-Pro-105.local>
2016-10-01 00:27:41 -0700

    Got trending hashTags

--- oauthtest1.py
+++ oauthtest1.py
@@ -12,14 +12,26 @@ ACCESS_TOKEN = twitter.obtain_access_token()
 
 twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
 
+toronto = twitter.get_place_trends(id=2487956)
+trend_array = []
+if toronto:
+   for trend in toronto[0].get('trends', []):
+      trend_array.append(trend['name'])
+
+#print trend_array
 longstring = ''''''
 
+search = trend_array[0]
+search = search[1:]
+
+#print search 
+
 #goes through results and print out the actual tweet
-results = twitter.search(q='AltRightStarWars', count=300)
+results = twitter.search(q=search, count=300)
 for result in results['statuses']:
     longstring += result['text']
     
 text_model = markovify.Text(longstring)
 
-for i in range(1, 10):
-    print text_model.make_short_sentence(140)
+twitter.update_status(status=text_model.make_short_sentence(140))
+
